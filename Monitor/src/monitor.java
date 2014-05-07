1787cb4afd765246eb9bae184f9af2996badafc6
javiergarduno <javier.adrian.martinez@gmail.com>
2014-05-07 12:17:29 -0500

    Descarga y compracion hecha con funciones

--- Monitor/src/monitor.java
+++ Monitor/src/monitor.java
@@ -12,6 +12,7 @@ import org.apache.commons.io.FileUtils;
 
 public class monitor {
 	PrintWriter writer;
+	static Area area;
 
 	public static void main(String[] args) throws IOException {
 		
@@ -24,68 +25,31 @@ public class monitor {
         });
 		
 		LogWriter("log.txt", "Inicio de Ejecución");
-		Area area = new Area();
+		area = new Area();
         
         URL url = new URL("http://intranet.pemex.com/os/pep/sdc/go/Reportes%20Diarios%20Histricos/Balance_de_Gas.pdf");
 		File newFile = new File("Balance_de_Gas.pdf");    
-		File oldFile = new  File ("Balance_de_Gas_anterior.pdf");		
+		File oldFile = new  File ("Balance_de_Gas_anterior.pdf");	
+		
+		URL urlOperativoCrudo = new URL("http://intranet.pemex.com/os/pep/sdc/go/Reportes%20Diarios%20Histricos/Resumen_Operativo_Crudo.xlsx");
+		File newOperativoCrudo = new File("Resumen_Operativo_Crudo.xlsx");
+		File oldOperativoCrudo = new File("Resumen_Operativo_Crudo_anterior.xlsx");
 	
 		try {
 	        while (true) {
-	           
-//	            Thread.sleep(5 * 1000);
-	            
+	        	
 	            System.out.println(new Date());       
 	            
 	            Download(url, newFile);
-		        if (FileUtils.contentEquals(newFile,oldFile)){
-		            System.out.println("Los archivos " +  newFile + " " + oldFile + " son iguales se elimina: " + newFile );
-		            LogWriter("log.txt","Los archivos" +  newFile + " " + oldFile + " son iguales se elimina: " + newFile);
-		        	FileUtils.forceDelete(newFile);		      
-		        	}
-		        else{
-		        	System.out.println("Los archivos son diferentes");
-		        	LogWriter("log.txt","Los archivos " +  newFile + " " + oldFile + " son diferentes");
-		        	
-		        	Calendar calendar = Calendar.getInstance();
-		    		//int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH); //cal.add(Calendar.DATE, -1);
-		    		int month = calendar.get(Calendar.MONTH);
-		    		String monthName = MonthName(month);
-		    		int year = calendar.get(Calendar.YEAR);
-		    		//test
-		    		
-		    		calendar.add(Calendar.DATE, -1);
-		    		int dayOfMonth_1 = calendar.get(Calendar.DAY_OF_MONTH);
-		    		System.out.println(dayOfMonth_1);
-		    		
-		    		
-		    		
-		    		String FileName =  "Balance_de_Gas " + dayOfMonth_1 + monthName + (year-2000) + ".pdf";    	
-		    	    File newFile2 = new  File(FileName);
-		    	    
-		    	    //Hace una copia del archivo nuevo con la fecha en el nombre
-		        	FileUtils.copyFile(newFile,newFile2 );
-		        	
-		        	System.out.println("El archivo \"" + newFile2 + "\" se ha guardado correctamente");
-		        	area.userText("El archivo \"" + newFile2 + "\" se ha guardado correctamente");
-		        	LogWriter("log.txt"," El archivo \"" + newFile2 + "\" se ha guardado correctamente");
-		        	
-		        	//test
-		        	/*
-		        	File directory = new File("D:\\Datos_Perfil\\241418\\Documents\\ADD");
-		        	FileUtils.copyFileToDirectory(newFile2, directory);
-		        	System.out.println("El archivo \"" + newFile2 + "\" se ha guardado correctamente en " + directory );
-		        	*/
-		        	
-		        	//Gurada el archivo nuevo como viejo
-		        	FileUtils.copyFile(newFile, oldFile);
-		        	//Elimina el archivo nuevo
-		        	FileUtils.forceDelete(newFile); 
-		        	
-		        	
-		        }
-		        //Thread.sleep(5 * 1000);
-		        Thread.sleep(60 * 1000);
+	            SaveFile(newFile,oldFile,BlanceGasFormtat());
+	            
+	            Download(url, newOperativoCrudo);
+	            SaveFile(newOperativoCrudo, oldOperativoCrudo, OperativoCrudoFormtat());
+	            
+	            
+		        
+		        Thread.sleep(5 * 1000);
+		        //Thread.sleep(60 * 1000);
 	        }
 	    } catch (InterruptedException e) {
 	        e.printStackTrace();
@@ -96,7 +60,6 @@ public class monitor {
 	static void Download(URL source,File fileToSave){
 		try {
             FileUtils.copyURLToFile(source, fileToSave);            
-            System.out.println("El archivo \"" + fileToSave + "\" se ha descargado");
             LogWriter("log.txt","El archivo \"" + fileToSave + "\" se ha descargado");
             
         } 
@@ -116,7 +79,22 @@ public class monitor {
 	       case 3:
 	    	   return "Abr";
 	       case 4:
-	    	   return "May";	      
+	    	   return "May";
+	       case 5:
+	    	   return "Jun";
+	       case 6:
+	    	   return "Jul";
+	       case 7:
+	    	   return "Ago";
+	       case 8:
+	    	   return "Sep";
+	       case 9:
+	    	   return "Oct";
+	       case 10:
+	    	   return "Nov";
+	       case 11:
+	    	   return "Dic";	       
+	    	   
 		}
 		return "";
 	}
@@ -127,5 +105,67 @@ public class monitor {
 		writer.println(new Date().toString() + "   " + text);
 		writer.close();*/	
 	}
+	
+	static String BlanceGasFormtat(){
+		
+		Calendar calendar = Calendar.getInstance();
+		//int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH); 
+		int month = calendar.get(Calendar.MONTH);
+		String monthName = MonthName(month);
+		int year = calendar.get(Calendar.YEAR);
+		
+		calendar.add(Calendar.DATE, -1);
+		int dayOfMonth_1 = calendar.get(Calendar.DAY_OF_MONTH);
+		
+		return "Balance_de_Gas " + dayOfMonth_1 + monthName + (year-2000) + ".pdf";
+	}
+	
+	static String OperativoCrudoFormtat(){
+		
+		Calendar calendar = Calendar.getInstance();
+		//int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH); 
+		int month = calendar.get(Calendar.MONTH);
+		String monthName = MonthName(month);
+		int year = calendar.get(Calendar.YEAR);
+		
+		//calendar.add(Calendar.DATE, -1);
+		int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+		
+		return "Resumen_operativoCrudo " + dayOfMonth + monthName + (year-2000) + ".xlsx";
+	}
 
+	static void SaveFile(File newFile, File oldFile, String fileName) throws IOException, InterruptedException{
+		
+		  if (FileUtils.contentEquals(newFile,oldFile)){
+	            System.out.println("Los archivos " +  newFile + " " + oldFile + " son iguales se elimina: " + newFile );
+	            LogWriter("log.txt","Los archivos" +  newFile + " " + oldFile + " son iguales se elimina: " + newFile);
+	        	FileUtils.forceDelete(newFile);		      
+	        	}
+	        else{
+	        	System.out.println("Los archivos son diferentes");
+	        	LogWriter("log.txt","Los archivos " +  newFile + " " + oldFile + " son diferentes");		        	
+	        	
+	    		   	
+	    	    File newFile2 = new  File(fileName);
+	    	    
+	    	    //Hace una copia del archivo nuevo con la fecha en el nombre
+	        	FileUtils.copyFile(newFile,newFile2 );
+	        	
+	        	area.userText("El archivo \"" + newFile2 + "\" se ha guardado correctamente");
+	        	LogWriter("log.txt"," El archivo \"" + newFile2 + "\" se ha guardado correctamente");
+	        	
+	        	//test
+	        	/*
+	        	File directory = new File("D:\\Datos_Perfil\\241418\\Documents\\ADD");
+	        	FileUtils.copyFileToDirectory(newFile2, directory);
+	        	System.out.println("El archivo \"" + newFile2 + "\" se ha guardado correctamente en " + directory );
+	        	*/
+	        	
+	        	//Gurada el archivo nuevo como viejo
+	        	FileUtils.copyFile(newFile, oldFile);
+	        	//Elimina el archivo nuevo
+	        	FileUtils.forceDelete(newFile); 
+	        }
+	       
+	}
 }
